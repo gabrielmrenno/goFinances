562d5b857ffdb6967423ac32c2fee8e47d892d30
Gabriel Martins Rennó <gabrielmrenno@gmail.com>
2022-05-07 00:20:48 -0300

    Erro de tipagem do FlatList

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+
+import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
+
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
+    HighlightCards,
+    TransactionList,
+    Transactions,
+    Title
+} from './styles';
+
+export interface DataListProps extends TransactionCardProps {
+    id: string;
+}
+
+export function Dashboard() {
+    const data: DataListProps[] = [
+        {
+            id: '1',
+            type: 'positive',
+            title: "Desenvolvimento de site",
+            amount: "R$ 12.000,00",
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            },
+            date: "13/04/2020"
+        },
+        {
+            id: '2',
+            type: 'negative',
+            title: "Almoço fora",
+            amount: "R$ 50,00",
+            category: {
+                name: 'Compras',
+                icon: 'coffee'
+            },
+            date: "13/04/2020"
+        },
+        {
+            id: '3',
+            type: 'negative',
+            title: "Desenvolvimento de site",
+            amount: "R$ 12.000,00",
+            category: {
+                name: 'Vendas',
+                icon: 'shopping-bag'
+            },
+            date: "13/04/2020"
+        }
+    ];
+    return (
+        <Container>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/18034920?v=4' }}
+                        />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Gabriel</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power" />
+                </UserWrapper>
+            </ Header>
+
+            <HighlightCards
+
+            >
+                <HighlightCard
+                    type="up"
+                    title="Entradas"
+                    amount="R$ 1.000.400,00"
+                    lastTransaction="Última entrada dia 13 de abril de 2022"
+                />
+                <HighlightCard
+                    type="down"
+                    title="Saídas"
+                    amount="R$ 17.400,00"
+                    lastTransaction="Última entrada dia 13 de abril de 2022"
+                />
+                <HighlightCard
+                    type="total"
+                    title="Total"
+                    amount="R$ 17.400,00"
+                    lastTransaction="Última entrada dia 13 de abril de 2022"
+                />
+            </HighlightCards>
+
+            <Transactions>
+                <Title> Listagem </Title>
+
+                <TransactionList
+                    data={data}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <TransactionCard data={item} />}
+                />
+            </Transactions>
+        </Container>
+    )
+}
