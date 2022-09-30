d61113fb69bd5d1fdeb3659078ba465ad1b125da
Gabriel Martins Rennó <gabrielmrenno@gmail.com>
2022-09-30 06:37:06 -0300

    Setting Google auth

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,5 +1,8 @@
-import { createContext, ReactNode, useContext } from 'react';
+import { createContext, ReactNode, useContext, useState } from 'react';
+import * as AuthSession from 'expo-auth-session';
 
+const { CLIENT_ID } = process.env;
+const { REDIRECT_URI } = process.env;
 
 interface AuthProviderProps {
     children: ReactNode;
@@ -12,16 +15,53 @@ interface User {
     photo?: string;
 }
 
-interface AuthContextData {
-    user: string;
-
+interface IAuthContextData {
+    user: User;
+    signInWithGoogle(): Promise<void>;
 }
 
-const AuthContext = createContext({} as AuthContextData);
+interface AuthorizationResponse {
+    params: {
+        access_token: string;
+    };
+    type: string;
+}
+
+
+const AuthContext = createContext({} as IAuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
+    const [user, setUser] = useState<User>({} as User);
+    async function signInWithGoogle() {
+
+        try {
+            const RESPONSE_TYPE = 'token';
+            const SCOPE = encodeURI('profile email');
+
+            const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+            const { type, params } = await AuthSession
+                .startAsync({ authUrl }) as AuthorizationResponse;
+
+            if (type === 'success') {
+                const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`);
+                const userInfo = await response.json();
+
+                setUser({
+                    id: userInfo.id,
+                    email: userInfo.email,
+                    name: userInfo.given_name,
+                    photo: userInfo.picture
+                });
+            }
+
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
+
     return (
-        <AuthContext.Provider value={{} as AuthContextData} >
+        <AuthContext.Provider value={{ user, signInWithGoogle }} >
             {children}
         </AuthContext.Provider>
     )
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Alert } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import {
     Container,
@@ -18,7 +19,16 @@ import LogoSvg from '../../@types/assets/logo.svg';
 import { SignInSocialButton } from '../../components/SignInSocialButton';
 
 export function SignIn() {
-    const data = useAuth();
+    const { signInWithGoogle } = useAuth();
+
+    async function handleSignInWithGoogle() {
+        try {
+            await signInWithGoogle();
+        } catch (err) {
+            console.log(err);
+            Alert.alert('Não foi possível conectar à conta google');
+        }
+    }
 
     return (
         <Container>
@@ -47,6 +57,7 @@ export function SignIn() {
                     <SignInSocialButton
                         title="Entrar com Google"
                         svg={GoogleSvg}
+                        onPress={handleSignInWithGoogle}
                     />
                     <SignInSocialButton
                         title="Entrar com Apple"
